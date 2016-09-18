6e65c2b8ea6ba9c2dc522b08fbace9879ac63bd4
Hugo Denizart <thepoon@cartooncraft.fr>
2016-09-18 17:40:26 +0200

    Calculate beatmap props with mods (except stars)
    Untested, should most likely work though.
    We need oppai to estimate star difficulty with mods.

--- src/main/java/lt/ekgame/storasbot/plugins/osu_top/MessageFormatter.java
+++ src/main/java/lt/ekgame/storasbot/plugins/osu_top/MessageFormatter.java
@@ -62,12 +62,12 @@ public class MessageFormatter {
 		scope.put("artist", Utils.escapeMarkdown(beatmap.getArtist()));
 		scope.put("title", Utils.escapeMarkdown(beatmap.getTitle()));
 		scope.put("version", Utils.escapeMarkdown(beatmap.getVersion()));
-		scope.put("od", decimalFormat.format(beatmap.getOverallDifficulty()));
-		scope.put("cs", decimalFormat.format(beatmap.getCircleSize()));
-		scope.put("hp", decimalFormat.format(beatmap.getHealthDrain()));
-		scope.put("ar", decimalFormat.format(beatmap.getApproachRate()));
-		scope.put("length", Utils.compactTimeString(beatmap.getTotalLength()));
-		scope.put("bpm", decimalFormat.format(beatmap.getBpm()));
+		scope.put("od", decimalFormat.format(beatmap.getOverallDifficulty(score.getMods())));
+		scope.put("cs", decimalFormat.format(beatmap.getCircleSize(score.getMods())));
+		scope.put("hp", decimalFormat.format(beatmap.getHealthDrain(score.getMods())));
+		scope.put("ar", decimalFormat.format(beatmap.getApproachRate(score.getMods())));
+		scope.put("length", Utils.compactTimeString(beatmap.getTotalLength(score.getMods())));
+		scope.put("bpm", decimalFormat.format(beatmap.getBpm(score.getMods())));
 		scope.put("stars", decimalFormat.format(beatmap.getStarDifficulty()));
 		scope.put("beatmap_link", String.format(BEATMAP_LINK, beatmap.getBeatmapId()));
 		scope.put("max_combo", String.valueOf(beatmap.getMaxCombo()));
